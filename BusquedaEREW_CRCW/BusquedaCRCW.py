00289b26e16c91b9dd81b1ba0ccf9272b8c6da20
loveemilianoo <mictlanmx205@gmail.com>
2026-03-11 22:53:04 -0600

    Busqueda CRCW con targets

--- BusquedaEREW_CRCW/BusquedaCRCW.py
+++ BusquedaEREW_CRCW/BusquedaCRCW.py
@@ -1,53 +1,54 @@
-import math
 import threading
 import time
 import random
 
-def minCRCW (L, win, n):
-    for i in range (n):
-        win[i] = 0
-
-    lock = threading.Lock()
-    
-    def comparador(i):
-        esMenor = True
-        for j in range(n):
-            if i != j and L[i] > L[j]:
-                esMenor = False
-                break
-        if not esMenor:
-            with lock:
-                win[i] = 1
-
+def minCRCW (L, n):
+    win = [0] * n
     hilos = []
+
+    def inicializar(idx):
+        win[idx] =0
+
     for i in range(n):
-        t = threading.Thread(target= comparador, args=(i,))
+        t = threading.Thread(target=inicializar, args=(i,))
         hilos.append(t)
         t.start()
-
     for t in hilos:
         t.join()
 
-    indexMin = None
-    for i in range (n):
-        if(win[i] == 0):
-            indexMin = i
+    hilosComp = []
+    def compararElementos (i,j):
+        if L[i] > L[j]:
+            win[i] = 1
+        elif L[i] < L[j]:
+            win[j] = 1
+
+    for i in range(n):
+        for j in range (i+1, n):
+            t = threading.Thread(target= compararElementos, args=(i,j))
+            hilosComp.append(t)
+            t.start()
+    for t in hilosComp:
+        t.join()
+
+    minimoIDX = -1
+    for i in range(n):
+        if win[i] == 0:
+            minimoIDX = i
             break
 
-    print("Indice Minimo: ",indexMin+1)
-    print ("RESULTADO: ",L[indexMin])
-    return L[indexMin]
+    return L[minimoIDX] if minimoIDX != -1 else None
 
 if __name__ == "__main__":
     n = 32
-    valores = random.sample(range(1, 100), n)
-    win = [0] * n
+    arreglo = random.sample(range(1, 100), n)
 
-    print("Mínimo en modelo CRCW")
-    print(f"\nLista L[0..{n-1}]: {valores}\n")
+    print("Minimo en modelo CRCW")
+    print(f"\nLista L[0..{n-1}]: {arreglo}\n")
 
     tiempoInicial = time.perf_counter()
-    resultado = minCRCW(valores, win, n)
+    resultado = minCRCW(arreglo, n)
     tiempoTotal = time.perf_counter() - tiempoInicial
 
+    print(f"El minimo es: {resultado}")
     print(f"\nTiempo de ejecución: {tiempoTotal:.6f} segundos") 
